e4ab33fdbf66d41d7c2c0b06cd2745d86088aaa5
Asher Shaheen <ashershaheen@gmail.com>
2017-09-11 11:28:13 -0400

    added methods for bcryot password in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+
+    attr_reader :password
+
+    def is_password?(password)
+        BCrypt::Password.new(password_digest).is_password?(password_attempt)
+    end
+
+    def password=(password)
+        @password = raw_password
+        self.password_digest = BCrypt::Password.create(raw_password)
+    end
 end
