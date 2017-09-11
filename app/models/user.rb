a97736ef2f2b3b4eca13a12edba1ddce452dd4b1
Asher Shaheen <ashershaheen@gmail.com>
2017-09-11 11:31:07 -0400

    added more methods for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,13 @@ class User < ApplicationRecord
 
     attr_reader :password
 
+    def self.find_by_credentials(username, password)
+        user.User.find_by(username: username)
+        return nil unless user
+        user if user.is_password?(password)
+        # user.is_password?(password) ? user : nil
+    end
+
     def is_password?(password)
         BCrypt::Password.new(password_digest).is_password?(password_attempt)
     end
