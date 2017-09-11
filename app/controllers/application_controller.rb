77861042d14671c1960a22299c88aac8a2acb995
Asher Shaheen <ashershaheen@gmail.com>
2017-09-11 11:45:43 -0400

    added user methods for sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  def sign_in(user)
+    token = SecureRandom.urlsafe_base64
+    session[:token] = token
+    # user.update!(session_token: token)
+    user.update_attribute(:session_token, token)
+  end
+
+  def sign_out
+    current_user.update_attribute(:session_token)
+    session.delete(:token)
+  end
+
+  def current_user
+    token = session[:token]
+    return nil if token.nil?
+    User.find_by(session_token: token)
+  end
+
 end
